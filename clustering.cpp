cea0889d42f0fb791c313ce7b37d57ceb8260f49
carloseduardov8 <carloseduardov8@hotmail.com>
2018-04-05 23:00:37 -0300

    Make threads faster than single line of execution

--- clustering.cpp
+++ clustering.cpp
@@ -6,7 +6,7 @@
 
 #define N 6000000			// Size of data
 #define K 20				// Dimension of data
-#define CORES 4				// Number of CPUs
+#define CORES 4 			// Number of CPUs
 
 using namespace std;
 
@@ -15,14 +15,15 @@ typedef double data_t;
 
 // Function declarations:
 data_t** generateRandomMatrix();
-void fillColumns(int threadId);
+void fillLines(int threadId, data_t** matrix);
 
 
 // Main program:
 int main(){
 
 	// Starts the stopwatch:
-	clock_t begin = clock();
+	struct timespec start, finish;
+    clock_gettime(CLOCK_MONOTONIC, &start);
 
 	// Loads the data matrix:
 	data_t** data = generateRandomMatrix();
@@ -33,11 +34,12 @@ int main(){
 	}
 
 	// Stops the stopwatch:
-	clock_t end = clock();
+	clock_gettime(CLOCK_MONOTONIC, &finish);
 
 	// Prints out elapsed time:
-	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-	cout << endl << "Elapsed time: " << time_spent << " seconds." << endl;
+    double elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+	cout << endl << "Elapsed time: " << elapsed << " seconds." << endl;
 
 }
 
@@ -53,9 +55,6 @@ data_t** generateRandomMatrix(){
 		matrix[i] = new data_t[K];
 	}
 
-	// Initializes random seed:
-	srand (time(NULL));
-
 
 	// Array of threads:
 	std::thread tasks[CORES];
@@ -63,7 +62,7 @@ data_t** generateRandomMatrix(){
 	// Loops through threads:
 	for (int threadId = 0; threadId < CORES; threadId++){
 		// Fires up thread to fill matrix:
-		tasks[threadId] = thread(fillColumns, threadId, matrix);
+		tasks[threadId] = thread(fillLines, threadId, matrix);
 	}
 
 	// Waits until all threads are done:
@@ -76,19 +75,24 @@ data_t** generateRandomMatrix(){
 
 }
 
-void fillColumns(int threadId, data_t** matrix){
+void fillLines(int threadId, data_t** matrix){
+
+    // Randomizes interval of data for each thread:
+    int interval = 15241*threadId % 200 - 100;
+
+    // Linear congruent generator:
+    int gen = (15245 + 12345) % interval;
 
 	// Number of columns to fill:
-	int each = K / CORES;
+	int each = N / CORES;
 
-	// Loops through designated columns:
-	for (int j = threadId*each; j < threadId*each + each; j++){
-		// Randomizes interval of data for each column:
-		int interval = rand() % 200 - 100;
-		// Loops through lines:
-		for (int i = 0; i < N; i++){
+	// Loops through designated lines:
+	for (int i = threadId*each; i < threadId*each + each; i++){
+		// Loops through columns:
+		for (int j = 0; j < K; j++){
 			// Fills in random data:
-			matrix[i][j] = (rand() % interval) - (interval / 2);
+            gen = (1664521*gen + 12341) % interval;
+			matrix[i][j] = gen;//(rand() % interval) - (interval / 2);
 		}
 	}
 
