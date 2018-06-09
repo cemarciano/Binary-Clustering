759be5939c984af29d03a4f811cb0d1103e49285
Carlos Eduardo Marciano <carloseduardov8@hotmail.com>
2018-06-09 16:47:06 -0300

    Change N and D sizes

--- include/global.h
+++ include/global.h
@@ -1,6 +1,6 @@
 typedef double data_t;
 
-#define N 6000000			// Size of data
-#define D 24				// Dimension of data
+#define N 1245005			// Size of data
+#define D 9					// Dimension of data
 #define K 2					// Number of divisions
 #define CORES 4 			// Number of CPUs
